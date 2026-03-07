6f392117575d92d40d89dc5d8a3a7f9ba5ed4bc2
Abdull811 <ge26z811@smail.iitm.ac.in>
2026-03-07 20:48:17 +0300

    Update inference.py

--- src/inference.py
+++ src/inference.py
@@ -7,7 +7,7 @@ import argparse
 import wandb
 import numpy as np
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
-from src.utils.data_loader import load_data
+from src.utils.load_data import load_data
 from src.ann.neural_network import NeuralNetwork
 
 def parse_arguments():
