ee419d70e15feebede0cce8e58a59b36b0b1ce67
Abdull811 <ge26z811@smail.iitm.ac.in>
2026-03-07 20:41:40 +0300

    Change model save path to best_model.npy

--- src/train.py
+++ src/train.py
@@ -113,7 +113,7 @@ def main(args=None):
 
         if val_acc > best_val_acc:
             best_val_acc = val_acc
-            np.save(args.model_save_path, model.get_weights())
+            np.save("src/best_model.npy", model.get_weights())
     
     # Training loss curve (keep plots)
     plt.figure()
