b2bb84299a5906aaf00eebcd05c060cf3ab288b7
Abdull811 <ge26z811@smail.iitm.ac.in>
2026-03-07 20:00:06 +0300

    Activation functions and add Softmax modified

--- src/ann/activations.py
+++ src/ann/activations.py
@@ -1,40 +1,43 @@
-"""Activation Functions and TIt's Derivatives
-Implements: ReLU, Sigmoid, Tanh
+"""
+Activation Functions and Their Derivatives
+Implements: ReLU, Sigmoid, Tanh, Softmax
 """
 
 import numpy as np
-# ReLu function
+
+# ReLU
 class ReLu:
-     def forward(self, z):
-          self.z = z
-          return np.maximum(0,z)
+    def forward(self, z):
+        self.z = z
+        return np.maximum(0, z)
      
-     def backward(self, da):
-         return da * (self.z > 0)
+    def backward(self, da):
+        return da * (self.z > 0)
 
-# Sigmoid function forward and backward    
+# Sigmoid
 class Sigmoid:    
-     def forward(self, z):
-         z = np.clip(z, -500, 500)
-         self.a = 1 / ( 1 + np.exp(-z))
-         return self.a
+    def forward(self, z):
+        z = np.clip(z, -500, 500)
+        self.a = 1 / (1 + np.exp(-z))
+        return self.a
      
-     def backward(self, da):
-         dz = da * self.a *(1 - self.a)
-         return dz
+    def backward(self, da):
+        dz = da * self.a * (1 - self.a)
+        return dz
         
-# Tanh activation function forward and backward
+# Tanh
 class Tanh:
-      def forward(self, z):
-          self.a = np.tanh(z)
-          return self.a
+    def forward(self, z):
+        self.a = np.tanh(z)
+        return self.a
 
-      def backward(self, da):
-          dz = da * (1 - self.a ** 2) 
-          return dz
+    def backward(self, da):
+        dz = da * (1 - self.a ** 2) 
+        return dz
 
-class Softmax(x):
-     def softmax(x):
-         x = x - np.max(x, axis=1, keepdims=True)
-         exp_x = np.exp(x)
-    return exp_x / np.sum(exp_x, axis=1, keepdims=True)
+# Softmax 
+class Softmax:
+    def forward(self, x):
+        x = x - np.max(x, axis=1, keepdims=True)
+        exp_x = np.exp(x)
+        return exp_x / np.sum(exp_x, axis=1, keepdims=True)
